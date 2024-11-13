f1846e8f5bde6dfa2a486070e14b5cb66dec0ac1
Muhammad Maarij Khan <157238100+maarijkhan24@users.noreply.github.com>
2024-11-13 11:12:24 +0500

    Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import styles from "./Nav.module.css";
 
 const Nav = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <div className={styles.navbar}>
       <div className={styles.navbarStart}>
         <div className={styles.dropdown}>
-          <div tabIndex={0} role="button" className={styles.dropdownButton}>
+          <button onClick={toggleDropdown} className={styles.dropdownButton} aria-label="Menu">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className={styles.icon}
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
-              aria-label="Menu"
             >
               <path
                 strokeLinecap="round"
@@ -23,18 +28,20 @@ const Nav = () => {
                 d="M4 6h16M4 12h8m-8 6h16"
               />
             </svg>
-          </div>
-          <ul className={styles.dropdownMenu}>
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="#about">About</Link>
-            </li>
-            <li>
-              <Link href="#skill">Skills</Link>
-            </li>
-          </ul>
+          </button>
+          {isDropdownOpen && (
+            <ul className={styles.dropdownMenu}>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="#about">About</Link>
+              </li>
+              <li>
+                <Link href="#skill">Skills</Link>
+              </li>
+            </ul>
+          )}
         </div>
         <Link href="/" className={styles.logo}>
           MK
