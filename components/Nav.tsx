861bf7bceb471084b32da4cc7e632dc8041b51fb
Muhammad Maarij Khan <157238100+maarijkhan24@users.noreply.github.com>
2024-11-13 11:26:08 +0500

    Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,4 +1,5 @@
-"use client"
+"use client";
+
 import React, { useState } from "react";
 import Link from "next/link";
 import styles from "./Nav.module.css"; // Assuming the CSS file is named Nav.module.css
