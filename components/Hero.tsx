81298a686a19a155a09aa6b16e5b1867a120533c
Muhammad Maarij Khan <157238100+maarijkhan24@users.noreply.github.com>
2024-11-13 11:01:20 +0500

    Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,33 +1,29 @@
 import React from "react";
 import Image from "next/image";
-import "./Hero.module.css"
+import styles from "./Hero.module.css";
 
 const Hero = () => {
   return (
     <div>
-      <section className="hero-section">
-        <div className="hero-container">
+      <section className={styles.heroSection}>
+        <div className={styles.container}>
           <Image
-            className="hero-image"
+            className={styles.heroImage}
             alt="hero"
             src="/image.png"
             width={500}
             height={450}
           />
-          <div className="hero-content">
-            <h1 className="hero-title">
-              Muhammad Maarij Najam
-            </h1>
-            <p className="hero-description">
+          <div className={styles.textContent}>
+            <h1 className={styles.title}>Muhammad Maarij Najam</h1>
+            <p className={styles.description}>
               A passionate Cloud & Cybersecurity expert with deep expertise in DevOps and AiOps, focused on driving secure and efficient solutions.
             </p>
-            <div className="hero-buttons">
-              <button className="hero-contact-button">
+            <div className={styles.buttonContainer}>
+              <button className={styles.contactButton}>
                 <a href="#contact">Contact</a>
               </button>
-              <button className="hero-explore-button">
-                Explore
-              </button>
+              <button className={styles.exploreButton}>Explore</button>
             </div>
           </div>
         </div>
