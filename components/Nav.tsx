92d409ee9e9493e63933af7337c6733457043f81
Muhammad Maarij Khan <157238100+maarijkhan24@users.noreply.github.com>
2024-11-13 11:09:25 +0500

    Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,67 +4,65 @@ import styles from "./Nav.module.css";
 
 const Nav = () => {
   return (
-    <div>
-      <div className={styles.navbar}>
-        <div className={styles.navbarStart}>
-          <div className={styles.dropdown}>
-            <div tabIndex={0} role="button" className={styles.dropdownButton}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className={styles.icon}
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-label="Menu"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </div>
-            <ul tabIndex={0} className={styles.dropdownMenu}>
-              <li>
-                <Link href="/">Home</Link>
-              </li>
-              <li>
-                <Link href="#about">About</Link>
-              </li>
-              <li>
-                <Link href="#skill">Skills</Link>
-              </li>
-            </ul>
+    <div className={styles.navbar}>
+      <div className={styles.navbarStart}>
+        <div className={styles.dropdown}>
+          <div tabIndex={0} role="button" className={styles.dropdownButton}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={styles.icon}
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-label="Menu"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
           </div>
-          <Link href="/" className={styles.logo}>
-            MK
-          </Link>
-        </div>
-        <div className={styles.navbarCenter}>
-          <ul className={styles.navMenu}>
+          <ul className={styles.dropdownMenu}>
             <li>
-              <Link href="/" className={styles.navLink}>
-                Home
-              </Link>
+              <Link href="/">Home</Link>
             </li>
             <li>
-              <Link href="#about" className={styles.navLink}>
-                About
-              </Link>
+              <Link href="#about">About</Link>
             </li>
             <li>
-              <Link href="#skill" className={styles.navLink}>
-                Skills
-              </Link>
+              <Link href="#skill">Skills</Link>
             </li>
           </ul>
         </div>
-        <div className={styles.navbarEnd}>
-          <Link href="#contact" className={styles.contactButton}>
-            Contact
-          </Link>
-        </div>
+        <Link href="/" className={styles.logo}>
+          MK
+        </Link>
+      </div>
+      <div className={styles.navbarCenter}>
+        <ul className={styles.navMenu}>
+          <li>
+            <Link href="/" className={styles.navLink}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="#about" className={styles.navLink}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="#skill" className={styles.navLink}>
+              Skills
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.navbarEnd}>
+        <Link href="#contact" className={styles.contactButton}>
+          Contact
+        </Link>
       </div>
     </div>
   );
